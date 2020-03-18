9dd22d2d8aa71ad622d5c9b086d146487b86b71c
吴霞 <2633659610@qq.com>
2020-03-18 12:24:34 +0800

    简单计算器中对于字符串的操作

---
+++ 简单计算器/简单计算器/Operation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 简单计算器
+{
+    class Operation
+    {
+
+        //public static int jiafa(int x, int y)
+        //{
+        //    return x + y;
+        //}
+        //public static int jiefa(int x, int y)
+        //{
+        //    return x - y;
+        //}
+        //public static int chengfa(int x, int y)
+        //{
+        //    return x * y;
+        //}
+        //public static int chufa(int x, int y)
+        //{
+        //    return x / y;
+        //}
+        public static string jiafa(string x, string y)
+        {
+            string m;
+            m = x + y;
+            StringBuilder MyStringBuilder = new StringBuilder();
+            MyStringBuilder.Append(x);
+            MyStringBuilder.Append(y);
+            m = MyStringBuilder.ToString();
+            return m;
+        }
+        public static string Jiefa(string x, string y)
+        {
+            string r = null;
+            // string source1 = "abscd";
+            // string source2 = "asd";
+            char[] ss = x.ToCharArray();
+            char[] bb = y.ToCharArray();
+            List<char> L = new List<char>();
+            for (int k = 0; k < ss.Length; k++)
+            {
+                L.Add(ss[k]);
+            }
+            for (int i = 0; i < bb.Length; i++)
+            {
+                for (int j = 0; j < ss.Length; j++)
+                {
+                    if (bb[i] == ss[j])
+                    {
+                        L.Remove(ss[j]);
+                    }
+                }
+            }
+
+            foreach (char c in L)
+            {
+                r = r + c;
+            }
+            return r;
+        }
+    }
+}
---
+++ 简单计算器/简单计算器/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 简单计算器
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please input x,y and (+/-/*//)");
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string m = Console.ReadLine();
+            switch (m)
+            {
+                case "+": { Console.WriteLine("输出的结果:{0}", Operation.jiafa(x, y)); } break;
+                case "-": { Console.WriteLine("输出的结果为：{0}", Operation.Jiefa(x, y)); } break;
+                    //case "*": Console.WriteLine("输出的结果为：{0}", Operation.chengfa(x, y)); break;
+                    //case "/": Console.WriteLine("输出的结果为：{0}", Operation.chufa(x, y)); break;
+            }
+        }
+    }
+}
