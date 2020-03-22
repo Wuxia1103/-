52b0f66fd1599c69682cc775f8a53fc4509a2075
吴霞 <2633659610@qq.com>
2020-03-22 17:25:40 +0800

    增加了对取余和按位与的运算

--- 简单计算器/简单计算器/Program.cs
+++ 简单计算器/简单计算器/Program.cs
@@ -10,16 +10,23 @@ namespace 简单计算器
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please input x,y and (+/-/*//)");
+            Console.WriteLine("please input x,y and (+/-/*/'/'/%/&)");
+
             string x = Console.ReadLine();
+            int X = int.Parse(x);
+      
             string y = Console.ReadLine();
+            int Y = int.Parse(y);
+            
             string m = Console.ReadLine();
             switch (m)
             {
-                case "+": { Console.WriteLine("输出的结果:{0}", Operation.jiafa(x, y)); } break;
-                case "-": { Console.WriteLine("输出的结果为：{0}", Operation.Jiefa(x, y)); } break;
-                    //case "*": Console.WriteLine("输出的结果为：{0}", Operation.chengfa(x, y)); break;
-                    //case "/": Console.WriteLine("输出的结果为：{0}", Operation.chufa(x, y)); break;
+                case "+": { Console.WriteLine("输出的结果:{0}", Operation.jiafa(X, Y)); } break;
+                case "-": { Console.WriteLine("输出的结果为：{0}", Operation.jiefa(X, Y)); } break;
+                case "*": Console.WriteLine("输出的结果为：{0}", Operation.chengfa(X, Y)); break;
+                case "/": Console.WriteLine("输出的结果为：{0}", Operation.chufa(X, Y)); break;
+                case "%": Console.WriteLine("输出的结果为：{0}", Operation.Quyu(X, Y)); break;
+                case "&": Console.WriteLine("输出的结果为：{0}", Operation.Anwei(X, Y)); break;
             }
         }
     }
